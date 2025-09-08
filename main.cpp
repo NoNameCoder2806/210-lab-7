f00aac9107d7f90e762cb3c1ced8442f0124cb4e
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-07 21:26:22 -0700

    Implemented the displayArray() function

--- main.cpp
+++ main.cpp
@@ -19,14 +19,14 @@ int main()
 {
     // Dynamically declare a string array that holds 5 string elements
     string* arr = nullptr;
-    arr = new string[5];
+    arr = new string[ARRAY_SIZE];
 
     // Assign 5 names to the array
     arr[0] = "Hoang";
     arr[1] = "Adam";
-    arr[2] = "";
-    arr[3] = "";
-    arr[4] = "";
+    arr[2] = "Brian";
+    arr[3] = "Victor";
+    arr[4] = "Aiden";
 
     // Display the original array
     cout << "Original array: ";
@@ -48,47 +48,29 @@ int main()
 
 // Function implementations
 /*
-    enterArrayData()
-    Prompts the user to enter the value of the 5 elements of the array
-    Arguments: a pointer to a double array
-    Returns: nothing
+    revsereArray()
+    Reverse the elements of the array
+    Arguments: a pointer to a string array
+    Returns: a string array pointer
 */
-string* reverseArray(string str)      // Let user input the elements for the double array
+string* reverseArray(string* str)      // Reverse the order of the array elements
 {
-    // Output a message to notify the user for the array entry
-    cout << "Data entry for the array:" << endl;
-
-    // Iterate 5 times and assign the entered values to the first 5 elements
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        // Prompt the user to enter the values for each element
-        cout << "    > Element #" << i << ": ";
-
-        // Store the value entered by the user
-        cin >> *(arr + i);
-        cin.ignore(1000, 10);
-    }
-
-    // Notify that the data entry is completed
-    cout << "Data entry complete!" << endl;
+    
 }
 
 /*
-    outputArrayData()
-    Display the content of a double array
-    Arguments: a pointer to a double array
+    displayArray()
+    Display the content of a string array
+    Arguments: a pointer to a string array
     Returns: nothing
 */
-void displayArray(string str)      // Output the elements of the array to the console
+void displayArray(string* str)      // Output the elements of the array to the console
 {
-    // Notify the outputting process
-    cout << "Outputting array elements: ";
-
-    // Iterate 5 times and output the values of the array elements
+    // Iterate 5 times and output the array elements
     for (int i = 0; i < ARRAY_SIZE; i++)
     {
         // Display the value
-        cout << *(arr + i);
+        cout << *(str + i);
 
         // If this is not the final element
         if (i != ARRAY_SIZE - 1)
@@ -100,26 +82,4 @@ void displayArray(string str)      // Output the elements of the array to the co
 
     // Enter a new line after displaying all the elements
     cout << endl;
-}
-
-/*
-    sumArray()
-    Calculates the sum of the array
-    Arguments: a pointer to the double array
-    Returns: the sum of the elements of the array
-*/
-double sumArray(double* arr)      // Calculate the sum of the array
-{
-    // Declare a new variable to hold the sum value
-    double sum = 0;
-
-    // Iterate through the array and calculate the sum
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        // Add the value of the current current element
-        sum += *(arr + i);
-    }
-
-    // Return the sum value
-    return sum;
 }
